c61a626669e30b68576b369ccbcfdd2bae05475d
jssatorres <juansataz.dev@gmail.com>
2023-10-20 19:24:24 +0200

    feat: import module

---
+++ index.js
@@ -0,0 +1,11 @@
+const pepe = require("./module/myModule")
+
+const {nameEx, pepeAge, anotherKey}= require("./module/myModule")
+
+// console.log(pepe)
+// console.log(nameEx, pepeAge, anotherKey)
+
+console.log(module);
+
+
+
